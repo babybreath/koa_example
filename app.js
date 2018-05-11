bb50d5c48555c2bd73a5dfbd9a00f052eb16bf2c
babybreath <jai2012@163.com>
2018-05-11 17:17:49 +0800

    change for eslint

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const Koa = require('koa');
-const app = new Koa();
 const koaStatic = require('koa-static');
 const bodyParser = require('koa-bodyparser');
 const logger = require('./lib/util/logger')(__filename);
@@ -8,12 +7,13 @@ const errorHandler = require('./lib/util/errorHandler');
 const router = require('./lib/router');
 const CONFIG = require('./config').conf;
 
-app.name = CONFIG.PROJECT_NAME;
-logger.info('run ' + app.name);
+const app = new Koa();
+
+logger.info(`NODE_ENV => ${process.env.NODE_ENV}`);
 
 if (CONFIG.STATIC_PATH) {
-  const staticPath = path.join(conf.ROOT_PATH, conf.STATIC_PATH);
-  logger.info(`静态资源目录 => {staticPath}`);
+  const staticPath = path.join(CONFIG.ROOT_PATH, CONFIG.STATIC_PATH);
+  logger.info(`静态资源目录 => ${staticPath}`);
   // 自定义 koa-static 配置
   app.use(koaStatic(staticPath));
 }
@@ -27,14 +27,14 @@ app.use(bodyParser());
 app.use(router.routes()).use(router.allowedMethods());
 
 // default
-app.use(async (ctx, next) => {
+app.use(async (ctx) => {
   logger.info(`${ctx.path} 未找到`);
-  ctx.status = 200;
+  ctx.status = 404;
   ctx.body = 'Not found.';
 });
 
 // 错误处理
-app.on('error', function(err, ctx) {
+app.on('error', (err) => {
   logger.error(err);
 });
 
